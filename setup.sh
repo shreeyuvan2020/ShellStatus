a4291266d19f193c1094bd9c55d901cce51c1f80
shreeyuvan2020 <63835553+shreeyuvan2020@users.noreply.github.com>
2025-08-13 14:22:44 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ kill -9 $(lsof -ti ":$PORT") 2>/dev/null
 git pull
 
 python3 -m venv .venv
-source .venv/bin/activate
-
+source Shells/bin/activate
+export
 pip install -r requirements.txt
 
 gunicorn -b ":$PORT" app:app
